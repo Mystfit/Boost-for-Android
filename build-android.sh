d25f6877728cf5d92dc7190379977fda4010993a
Byron Mallett <byronated@gmail.com>
2020-03-09 15:43:09 +1300

    Added ndk21

--- build-android.sh
+++ build-android.sh
@@ -351,7 +351,7 @@ case "$NDK_RN" in
 		CXXPATH=$AndroidNDKRoot/toolchains/${TOOLCHAIN}/prebuilt/${PlatformOS}-x86_64/bin/clang++
 		TOOLSET=clang
 		;;
-	"19.0"|"19.1"|"19.2"|"20.0"|"20.1")
+	"19.0"|"19.1"|"19.2"|"20.0"|"20.1"|"21")
 		TOOLCHAIN=${TOOLCHAIN:-llvm}
 		CXXPATH=$AndroidNDKRoot/toolchains/${TOOLCHAIN}/prebuilt/${PlatformOS}-x86_64/bin/clang++
 		TOOLSET=clang
